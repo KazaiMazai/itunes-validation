1d207d80458f34c29a03a81e392a934ca83737ca
Stuart Breckenridge <me@stuartbreckenridge.com>
2017-07-20 07:28:06 +0800

    make it better

---
+++ api/0.1/sandbox.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var router = express.Router();
+
+
+
+// What to do when a validation post request is received.
+router.get('/', function (req, res) {
+
+    // Check for receipt data
+    if (req.query.receipt !== undefined) {
+
+        var r = req.query.receipt;
+        var s = req.query.secret;
+        var e = req.query.excluded || true;
+
+        request(
+            {
+                method: 'POST', uri: 'https://sandbox.itunes.apple.com/verifyReceipt',
+                body: JSON.stringify({'receipt-data': r, 'password': s, 'exclude-old-transactions': e})
+            }, function (error, response, body) {
+
+                if (response.statusCode === 200) {
+                    res.status(response.statusCode).send(response.body);
+                    console.log('200 response');
+                    console.log(body)
+                } else {
+                    res.status(response.statusCode).send(response.status);
+                    console.log(body)
+                }
+            }
+        )
+    } else {
+        res.status(400).send('Receipt data is missing.');
+    }
+
+});
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var sandbox = require('./api/0.1/sandbox');
+var production = require('./api/0.1/production');
+var app = express();
+
+app.set('port', (process.env.PORT || 8443));
+
+app.use('/0.1/sandbox', sandbox);
+app.use('/0.1/validate', production);
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
